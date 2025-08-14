c9032ad94e33dd7de29b04c2614fbbb7bb302406
CYB-LAB <18943393164@163.com>
2025-08-14 11:07:56 +0800

    优化网关登录校验异常提示信息

---
+++ xiaohongshu-gateway/src/main/java/com/itpractice/xiahongshu/gateway/enums/ResponseCodeEnum.java
@@ -0,0 +1,28 @@
+package com.itpractice.xiahongshu.gateway.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author cyb
+ *
+ * 响应码枚举
+ */
+
+@Getter
+@AllArgsConstructor
+public enum ResponseCodeEnum {
+
+    // ----------- 通用异常状态码 -----------
+    SYSTEM_ERROR("500", "系统繁忙，请稍后再试"),
+    UNAUTHORIZED("401", "权限不足"),
+
+    // ----------- 业务异常状态码 -----------
+
+    ;
+
+    // 异常码
+    private final String errorCode;
+    // 错误信息
+    private final String errorMessage;
+}
