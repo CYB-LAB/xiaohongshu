3e8f276bd2ec08cd1b6061e95aeacf42d7b0a908
CYB-LAB <18943393164@163.com>
2025-08-15 10:04:35 +0800

    搭建对象存储服务的基础骨架

---
+++ xiaohongshu-oss/xiaohongshu-oss-biz/src/main/java/com/itpractice/xiaohongshu/oss/biz/XiaohongshuOssBizApplication.java
@@ -0,0 +1,11 @@
+package com.itpractice.xiaohongshu.oss.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohongshuOssBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuOssBizApplication.class, args);
+    }
+}
