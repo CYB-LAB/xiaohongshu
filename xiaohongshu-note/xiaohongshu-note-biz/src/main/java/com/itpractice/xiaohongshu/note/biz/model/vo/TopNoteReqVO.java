2f97bde9126dd46452e506ee0326c5b1b92b23e7
CYB-LAB <18943393164@163.com>
2025-08-20 14:25:39 +0800

    笔记指定/取消置顶接口开发

---
+++ xiaohongshu-note/xiaohongshu-note-biz/src/main/java/com/itpractice/xiaohongshu/note/biz/model/vo/TopNoteReqVO.java
@@ -0,0 +1,26 @@
+package com.itpractice.xiaohongshu.note.biz.model.vo;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author cyb
+ *
+ * 笔记置顶/取消置顶 请求参数  入参
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TopNoteReqVO {
+
+    @NotNull(message = "笔记 ID 不能为空")
+    private Long id;
+
+    @NotNull(message = "置顶状态不能为空")
+    private Boolean isTop;
+}
