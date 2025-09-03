43411f93d3f437c0ed074ff197ceb797d24e7f72
CYB-LAB <18943393164@163.com>
2025-09-03 16:23:28 +0800

    整合canal--实现binlog数据消费

--- xiaohongshu-search/src/main/java/com/itpractice/xiaohongshu/search/XiaohongshuSearchApplication.java
+++ xiaohongshu-search/src/main/java/com/itpractice/xiaohongshu/search/XiaohongshuSearchApplication.java
@@ -2,8 +2,10 @@ package com.itpractice.xiaohongshu.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class XiaohongshuSearchApplication {
     public static void main(String[] args) {
         SpringApplication.run(XiaohongshuSearchApplication.class, args);
