2ca8574b929d153fd20b6bd0be0750fbcdae3357
CYB-LAB <18943393164@163.com>
2025-09-04 09:44:09 +0800

    评论服务搭建

---
+++ xiaohongshu-comment/xiaohongshu-comment-biz/src/main/java/com/itpractice/xiaohongshu/comment/biz/domain/XiaohongshuCommentBizApplication.java
@@ -0,0 +1,13 @@
+package com.itpractice.xiaohongshu.comment.biz.domain;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.itpractice.xiaohongshu.comment.biz.domain.mapper")
+public class XiaohongshuCommentBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuCommentBizApplication.class, args);
+    }
+}
